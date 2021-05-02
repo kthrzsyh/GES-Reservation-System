50f4270352125743d586df44efd40d6f56ff600d
Achmad Kathariza <56335905+kthrzsyh@users.noreply.github.com>
2021-05-02 18:18:00 +0700

    hero-page

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,43 +7,43 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link href="/css/style.css" type="text/css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    
+
     <title>GBI Emunah Solo Baru</title>
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<body class="backgroundku">
+    <div style="height: 75px;"></div>
+    <nav class="navbar navbar-expand-lg navbarku fixed-top transparent navbar-inverse">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">GBI Emunah Solo Baru</a>
+            <a href="/"><img src="{{asset('/img/logo_ges.png')}}" style="height: 50px;" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
                 </ul>
                 <div class="d-flex">
                     @if(auth()->check())
                     @if(auth()->user()->role == 'admin')
-                    <a href="/admin" class="btn btn-outline-danger" style="margin-right: 20px;" type="submit">Dashboard</a>
+                    <a href="/admin" class="bt-primary-navbar" style="margin-right: 20px;" type="submit">Dashboard</a>
                     @else
-                    <a href="/member" class="btn btn-outline-danger" style="margin-right: 20px;" type="submit">Dashboard</a>
+                    <a href="/member" class="bt-primary-navbar" style="margin-right: 20px;" type="submit">Dashboard</a>
                     @endif
-                    <a href="/logout" class="btn btn-outline-success" style="margin-right: 20px;" type="submit">Logout</a>
+                    <a href="/logout" class="bt-primary-navbar" style="margin-right: 20px;" type="submit">Logout</a>
                     @else
-                    <a href="/login" class="btn btn-outline-success" style="margin-right: 20px;" type="submit">Login</a>
-                    <a href="/pendaftaran" class="btn btn-outline-success" type="submit">Daftar</a>
+                    <a href="/login" class="bt-primary-navbar" style="margin-right: 20px;" type="submit">Login</a>
+                    <a href="/pendaftaran" class="bt-primary-navbar" type="submit">Daftar</a>
                     @endif
 
                 </div>
             </div>
         </div>
     </nav>
-    <div class="mb-4 mt-4">
+    <div class="mt-4">
         @yield('content')
     </div>
 
-    <div style="height: 200px; background-color: dimgrey;">
+    <div style="height: 100px; background-color: var(--primary_color);">
         <div class="d-flex justify-content-center">
             <div class="col pt-4">
                 <div class="text-center">
@@ -53,16 +53,23 @@
                     <h6 class="footer">Created by : Yeremia Tacha</h6>
                 </div>
             </div>
-
-
-
         </div>
 
     </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(function() {
+            $(document).scroll(function() {
+                var $nav = $(".navbar");
+                $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+
+            });
+        });
+    </script>
     @yield('js')
 </body>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,59 @@
 @extends('base')
 @section('content')
 <div class="container">
-    <div class="row">
-    @foreach($event as $item)
-    <div class="col col-lg-3">
-    <div class="card">
-            <img src="/img/sample.jpeg" class="card-img-top" alt="gambar_event">
-            <div class="card-body">
-                <h5 class="card-title">{{$item->nama}}</h5>
-                <p class="card-text">{{$item->deskripsi}}</p>
+    <div class="hero-page">
+        <div class="row">
+            <div class="col-md-6 d-flex ">
+                <div class="align-self-center">
+                    <p class="title-hero">Reservasi Di Sini!</p>
+                    <p class="desc-hero">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                    <a href="" class="bt-secondary" style="margin-top: 16px; display: inline-block">Reservasi Sekarang!</a>
+                </div>
             </div>
-            <div style="display: flex; justify-content: space-between;">
-                <div>Kuota</div>
-                <div>{{$item->kuota}}</div>
-            </div>
-            <div style="display: flex; justify-content: space-between;">
-                <div>Tanggal Event</div>
-                <div>{{$item->tanggal}}</div>
-            </div>
-            <div style="display: flex; justify-content: space-between;">
-                <div>Batas Pendaftaran</div>
-                <div>{{$item->expired}}</div>
-            </div>
-            <!-- <ul class="list-group list-group-flush">
-                <li class="list-group-item">{{$item->kuota}}</li>
-                <li class="list-group-item">{{$item->tanggal}}</li>
-                <li class="list-group-item">{{$item->expired}}</li>
-            </ul> -->
-            <div class="card-body">
-                <a href="#" class="btn btn-primary">Daftar</a>
-                <a href="/detail/{{$item->id}}" class="btn btn-success">Detail</a>
+            <div class="col-md-6 d-flex justify-content-center">
+                <img src="{{asset('/img/hero-img.png')}}" style="height: 350px;" alt="">
             </div>
         </div>
     </div>
-    @endforeach
-        
-        
+</div>
+<div class="bg-white" style="padding-top: 50px; padding-bottom: 50px">
+    <div class="container">
+        <div>
+            <p style="font-size: 2rem; font-weight: bold;">Event yang Akan Datang</p>
+        </div>
+        <div class="row">
+            @foreach($event as $item)
+            <div class="col col-lg-3">
+                <div class="cardku">
+                    <img src="/img/sample.jpeg" class="card-img-top" alt="gambar_event">
+                    <p class="card-title">{{$item->nama}}</p>
+                    <p class="card-text">{{$item->deskripsi}}</p>
+                    <div class="keterangan">
+                        <table>
+                            <tr>
+                                <td>Available</td>
+                                <td style="margin-right: 5px;">: </td>
+                                <td>{{$item->kuota}} Seat(s)</td>
+                            </tr>
+                            <tr>
+                                <td>Date</td>
+                                <td style="margin-right: 5px;">: </td>
+                                <td>{{$item->tanggal}}</td>
+                            </tr>
+                            <tr>
+                                <td>Registration Limit</td>
+                                <td style="margin-right: 5px;">:</td>
+                                <td>{{$item->expired}}</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <a href="/detail/{{$item->id}}" class="bt-primary d-flex justify-content-center align-items-center" style="width: 100%; height: 50px; ">Detail</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
 
-
 </div>
+
 @endsection
